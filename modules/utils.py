1a6a01376dd92fc1878f1ea956a5b641b84fb100
Kevin Lu <kevinlu1248@gmail.com>
2020-07-26 22:04:43 -0700

    Added indirectly cached property

--- modules/utils.py
+++ modules/utils.py
@@ -20,3 +20,22 @@ def cached_property(property_function: Callable) -> Callable:
         return getattr(self, prop_class_name)
 
     return decorated_property
+
+
+def indirectly_cached_property(property_function: Callable) -> Callable:
+    """
+    Like cached property but runs another function that indirectly sets the 
+    private variable to the solution.
+    """
+
+    @property
+    def decorated_property(self):
+        prop_name = "__{}".format(property_function.__name__)
+        prop_class_name = "_{}__{}".format(
+            type(self).__name__, property_function.__name__
+        )
+        if not hasattr(self, prop_class_name):
+            property_function(self)
+        return getattr(self, prop_class_name)
+
+    return decorated_property
