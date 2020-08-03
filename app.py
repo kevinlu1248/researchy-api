075caf882a8fa153f0f3bfa0ee39896056a96d59
Kevin Lu <kevinlu1248@gmail.com>
2020-08-02 23:52:39 -0700

    More htmls made

--- app.py
+++ app.py
@@ -1,13 +1,28 @@
 from flask import Flask, request, render_template, make_response
-# from flask_restful import Resource, Api
 from modules.website import Website
+from dotenv import load_dotenv
 
+from db import *
+
+load_dotenv()
 app = Flask("researchy-api")
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+@app.route("/login")
+def login():
+    return render_template("index.html")
+
+@app.route("/signup")
+def signup():
+    if request.method == "GET":
+        return render_template("signup.html")
+    else if request.method == "POST":
+        return render_template("signup.html")
+
+# THE API
 @app.route("/api", methods=["POST"])
 def api():
     body = request.form
@@ -18,26 +33,6 @@ def api():
         return make_response("Please provide a text or url.", 400)
     display = Website(url=body.get("url", None), raw_html=body.get("text", None))
     return make_response(display.description, 200)
-
-# api = Api(app)
-
-# class ResearchyAPI(Resource):
-#     def get(self):
-#         # Placeholder
-#         return render_template("index.html")
-
-    # def post(self):
-    #     body = request.form
-    #     print(body)
-    #     if not body:
-    #         return make_response("Please provide a JSON object.", 400)
-    #     if "text" not in body and "url" not in body:
-    #         return make_response("Please provide a text or url.", 400)
-    #     display = Website(url=body.get("url", None), raw_html=body.get("text", None))
-    #     return make_response(display.description, 200)
-
-
-# api.add_resource(ResearchyAPI, "/")
 
 # running on https://researchy-api.herokuapp.com
 # running on http://64.225.115.179/
