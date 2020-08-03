5545e788b9ec1bc68329da8968fd45b93df5feaa
Kevin Lu <kevinlu1248@gmail.com>
2020-08-02 23:20:08 -0700

    Added templates

--- app.py
+++ app.py
@@ -1,29 +1,46 @@
-from flask import Flask, request, make_response
-from flask_restful import Resource, Api
+from flask import Flask, request, render_template, make_response
+# from flask_restful import Resource, Api
 from modules.website import Website
 
 app = Flask("researchy-api")
-api = Api(app)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/api", methods=["POST"])
+def api():
+    body = request.form
+    print(body)
+    if not body:
+        return make_response("Please provide a valid object.", 400)
+    if "text" not in body and "url" not in body:
+        return make_response("Please provide a text or url.", 400)
+    display = Website(url=body.get("url", None), raw_html=body.get("text", None))
+    return make_response(display.description, 200)
+
+# api = Api(app)
+
+# class ResearchyAPI(Resource):
+#     def get(self):
+#         # Placeholder
+#         return render_template("index.html")
+
+    # def post(self):
+    #     body = request.form
+    #     print(body)
+    #     if not body:
+    #         return make_response("Please provide a JSON object.", 400)
+    #     if "text" not in body and "url" not in body:
+    #         return make_response("Please provide a text or url.", 400)
+    #     display = Website(url=body.get("url", None), raw_html=body.get("text", None))
+    #     return make_response(display.description, 200)
 
 
-class ResearchyAPI(Resource):
-    def get(self):
-        # Placeholder
-        return "An API for making an HTML document more legible and highlights key words. Please POST to use this according to https://github.com/kevinlu1248/researchy-api."
+# api.add_resource(ResearchyAPI, "/")
 
-    def post(self):
-        body = request.form
-        print(body)
-        if not body:
-            return make_response("Please provide a JSON object.", 400)
-        if "text" not in body and "url" not in body:
-            return make_response("Please provide a text or url.", 400)
-        display = Website(url=body.get("url", None), raw_html=body.get("text", None))
-        return make_response(display.description, 200)
-
-
-api.add_resource(ResearchyAPI, "/")
 # running on https://researchy-api.herokuapp.com
+# running on http://64.225.115.179/
 
 if __name__ == "__main__":
     app.run(debug=False, port=5001)
---
+++ db.py
@@ -0,0 +1,26 @@
+import sqlalchemy
+from sqlalchemy import create_engine, Column, Integer, String, Sequence
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+engine = create_engine(
+    "postgres://vcommvznlesvvf:306a5c86a2c5b206725287d7231a26563d8ad1fc6289173261218b8312485e81@ec2-3-208-50-226.compute-1.amazonaws.com:5432/dde9qpdu2gb4s2",
+    echo=True,
+)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = "users"
+    user_id = Column(Integer, Sequence("user_id_seq"), primary_key=True)
+    email = Column(String(255))
+    password = Column(String(255))
+
+    def __repr__(self):
+        return "<User(email='{}', password='{}')>".format(
+            self.email,
+            self.password,
+        )
