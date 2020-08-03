ab7ea754af26a6f416f9f8852920875175bcff71
Kevin Lu <kevinlu1248@gmail.com>
2020-08-02 19:53:55 -0700

    Test deploy

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ cd /root/researchy-api
 source venv/bin/activate
 pip3 install -r requirements.txt
 pkill gunicorn
-gunicorn --bind 0.0.0.0:80 app:app
+screen -S api gunicorn --bind 0.0.0.0:80 app:app
