87b8a8ca9181d6bb2ae5122c9cb735e50f53e06a
Kevin Lu <kevinlu1248@gmail.com>
2020-08-08 12:09:15 -0700

    restructuring

--- app.py
+++ app.py
@@ -1,11 +1,35 @@
-from flask import Flask, request, render_template, make_response
-from modules.website import Website
+import os
+
+from flask import Flask, request, render_template, make_response, redirect
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+# from modules.website import Website 
 from dotenv import load_dotenv
 
-from db import *
+from db import engine, User
 
 load_dotenv()
 app = Flask("researchy-api")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URL")
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    __tablename__ = "users"
+    user_id = db.Column(db.Integer, db.Sequence("user_id_seq"), primary_key=True)
+    first = db.Column(db.String(255))
+    last = db.Column(db.String(255))
+    email = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password(self.password_hash, password)
+
+    def __repr__(self):
+        return "<User(name='{} {}' email='{}')>".format(self.first, self.last, self.email)
+
 
 @app.route("/")
 def index():
@@ -15,12 +39,21 @@ def index():
 def login():
     return render_template("index.html")
 
-@app.route("/signup")
+@app.route("/signup", methods=["GET", "POST"])
 def signup():
+    print(request.method )
     if request.method == "GET":
         return render_template("signup.html")
-    else if request.method == "POST":
-        return render_template("signup.html")
+    elif request.method == "POST":
+        # TODO: ADD FORM VALIDATION
+        if db.session.query(User.user_id).filter_by(email=request.form['email']).scalar() is None:
+            new_user = User(email=request.form['email'], password="")
+            new_user.set_password(request.form['password'])
+            db.session.add(new_user)
+            db.session.commit()
+            return redirect("/", 200) # success
+        else:
+            return redirect("/signup", 200) # failure
 
 # THE API
 @app.route("/api", methods=["POST"])
